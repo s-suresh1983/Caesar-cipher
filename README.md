c7ea2646815036f12729b547042b5e474716e1b3
suresh babu <46187428+s-suresh1983@users.noreply.github.com>
2022-06-01 13:30:32 +0530

    commit_1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Caesar cipher
  A Caesar cipher is a simple encryption technique. The encryption using a Ceasar cipher replaces a letter in the plain text with a letter that is a fixed number down in the alphabet. For example, with a shift of 5
+A Caesar cipher is a simple encryption technique. The encryption using a Ceasar cipher replaces a letter in the plain text with a letter that is a fixed number down in the alphabet. For example, with a shift of 5 the following substitutions would take place:
+
+a → f
+b → g
+c → h
+…
+v → a
+w → b
+…
+z → e
+Using this substitutions, a plain text can be encrypted:
---
+++ code.py
@@ -0,0 +1,37 @@
+encry_dict = {
+    "a": "f",
+    "b": "g",
+    "c": "h",
+    "d": "i",
+    "e": "j",
+    "f": "k",
+    "g": "l",
+    "h": "m",
+    "i": "n",
+    "j": "o",
+    "k": "p",
+    "l": "q",
+    "m": "r",
+    "n": "s",
+    "o": "t",
+    "p": "u",
+    "q": "v",
+    "r": "w",
+    "s": "x",
+    "t": "y",
+    "u": "z",
+    "v": "a",
+    "w": "b",
+    "x": "c",
+    "y": "d",
+    "z": "e",
+}
+sentence = input('Please enter a sentence:')
+sentence = sentence.lower()
+encrypted_sentence = ''
+for char in sentence:
+    if char in encry_dict:
+        encrypted_sentence += encry_dict[char]
+    else:
+        encrypted_sentence += char
+print('The encrypted sentence is:', encrypted_sentence)
